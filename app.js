4dd8fdd19cb86545e0de56e8e219d0632ba7458b
ValeronChareuka <vchareuka8@gmail.com>
2021-03-26 20:50:51 +0300

    refactor: rename classes by rule 3.2

--- app.js
+++ app.js
@@ -9,9 +9,9 @@
 // Event handling, user interaction is what starts the code execution.
 
 var taskInput=document.getElementById("new-task");//Add a new task.
-var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var addButton=document.getElementById("add-task");//first button
+var incompleteTaskHolder=document.getElementById("unfinished-tasks");//ul of #incompleteTasks
+var completedTasksHolder=document.getElementById("finished-tasks");//completed-tasks
 
 
 //New task list item
