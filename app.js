c2f3ff9d10ccea25806b1055dc6b8966b1abc051
ValeronChareuka <vchareuka8@gmail.com>
2021-03-27 17:03:57 +0300

    fix: separat selectors and properties by rule 3.8

--- app.js
+++ app.js
@@ -87,7 +87,7 @@ var editTask=function(){
     var listItem=this.parentNode;
 
     var editInput=listItem.querySelector('.input');
-    var label=listItem.querySelector("label");
+    var label=listItem.querySelector(".label");
     var editBtn=listItem.querySelector(".edit");
     var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .editmode
